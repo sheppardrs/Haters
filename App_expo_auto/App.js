05e63c560db2ce75c37119fd4bb0b9f928eb3971
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-01-04 15:15:43 -0500

    added style file, created componenets dir, began adding firebase

--- App_expo_auto/App.js
+++ App_expo_auto/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+
+// import styles
+const styles = require('./styles.js')
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "<your-storage-bucket>",,
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class App extends React.Component {
   render() {
@@ -11,11 +25,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f2f2f2',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
